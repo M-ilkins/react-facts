634d575340833b332286502200f6f9f47200cce7
Matthew Wilkins <wilkins52matthew@gmail.com>
2022-04-28 16:08:06 -0400

    finish project-add content, add css

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import styles from '../../../src/style.css';
 
 export default function Main () {
     return (
-        <div>
-          <h1>This is the main content</h1>
-          <ul>
-            <li>This is an li element</li>
+        <div className="main-content">
+          <h1>Fun facts about React</h1>
+          <ul className="facts-list">
+            <li>Was first released in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100K stars on GitHub</li>
+            <li>Is maintained by Facebook</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
           </ul>
         </div>
     )
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import reactLogo from '../../images/Group.png';
+import reactText from '../../images/ReactFacts.png';
+import styles from '../../../src/style.css';
 
 export default function Navbar(){
     return (
         <nav>
-            <h1>Navbar goes here!</h1>
+            <img src={reactLogo} className="react-logo"></img>
+            <img src={reactText} className="react-logo-text"></img>
+            <h3>React Course - Project 1</h3>
         </nav>
     )
 };
